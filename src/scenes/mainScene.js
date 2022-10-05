8185e3af6d349396347624d69c9b31bc28cf3f64
Anton Knysh <anton@profairgames.com>
2022-10-05 08:08:01 +0300

    Created the main animation in one file

---
+++ src/scenes/mainScene.js
@@ -0,0 +1,13 @@
+// export default class GameScene extends Phaser.Scene {
+//   constructor() {
+//     super('GameScene')
+//   }
+
+//   preload() {}
+
+//   create() {
+//     g.circle = this.add.circle(config.width/2, config.height/2, 20, 0xffffff);
+//   }
+
+//   update() {}
+// }
